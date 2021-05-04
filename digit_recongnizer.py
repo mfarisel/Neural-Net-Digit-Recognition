00bb15b11f5220fc1a067eccc0b880877c7ec495
mfarisel <73967853+mfarisel@users.noreply.github.com>
2021-05-03 18:09:57 -0700

    adding files and updating digit_recognizer

--- digit_recongnizer.py
+++ digit_recongnizer.py
@@ -14,8 +14,9 @@ root.resizable(width=False, height=False)
 # This sets the size of the window
 root.geometry('600x400')
 root.configure(bg='gray')
-
+# Setting the size and background color of the canvas
 canvas = tk.Canvas(root,width =400, height =400, bg = 'white')
+# Placing the canvas in the top left corner
 canvas.place(relx=0,rely=0)
 
 def clear_canvas():
@@ -23,20 +24,40 @@ def clear_canvas():
 
 def save():
 
-    #img_data = io.BytesIO()
+    # Saving the canvas as a Postscript
     ps = t.getscreen().getcanvas().postscript(colormode = 'gray')
-    #Need to figure out how to conver postscript to numpy
+    # Converting postscript to image.postscript object
     img = Image.open(io.BytesIO(ps.encode('utf-8')))
-    #pix = np.asarray(img)
-    #img2 = img.convert(mode='RGB')
-    img2 = img.convert()
+    #converting image to Image.image object
+    img = img.convert()
+    #resizing image to be the appropriate input size for NN
+    img = img.resize((28,28), Image.ANTIALIAS)
+    #Converting the image to an array
+    pix = np.array(img)
     
-    pix = np.array(img2)
-    imgplot = plt.imshow(pix)
-    plt.show()
+    
+    #Grayscale = R / 3 + G / 3 + B / 3.
+    #Grayscale  = 0.299R + 0.587G + 0.114B
+    
+    #here we are converting the image to be grayscale
+    
+    R = pix[:,:,0]
+    G = pix[:,:,1]
+    B = pix[:,:,2]
+    
+    greyscale_img = np.zeros((pix.shape[0],pix.shape[1]))
+    
+    for i in range(pix.shape[0]):
+        for j in range(pix.shape[1]):
+            greyscale_img[i][j] = (R[i][j]/3)+(B[i][j]/3)+(G[i][j]/3)
+            greyscale_img[i][j] = 255 - greyscale_img[i][j]
 
     
-    print(pix.shape)
+    #imgplot = plt.imshow(greyscale_img, cmap='gray')
+    #plt.show()
+    
+    print('hi')
+
     
 
 
@@ -46,10 +67,13 @@ def dragging(x, y):  # These parameters will be the mouse position
     t.setheading(t.towards(x, y))
     t.goto(x, y)
     t.ondrag(dragging)
+    
+
 
 def set_mouse_pos(x,y):
     t.up()
     t.goto(x,y)
+    
 
 
 screen = turtle.TurtleScreen(canvas)
@@ -60,7 +84,7 @@ t = turtle.RawTurtle(screen)
 
 t.pencolor("black") 
 t.speed(speed=0)
-t.width(width=10)
+t.width(width=40)
 
 t.ondrag(dragging)
 screen.onscreenclick(set_mouse_pos)
@@ -74,4 +98,4 @@ button_save.place(relx=.75, rely=.6)
 
 root.mainloop()
 
-#python tkinter2.py
+#python digit_recongnizer.py
